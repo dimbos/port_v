dc0bea134df4e03c36e65b1c0950aadd7f795abc
dimbos <dimboss@list.ru>
2018-12-12 22:04:11 +0300

    verstka

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+    "use strict";
+    $('#info').click(function(){
+        $('#info_box').toggleClass('open');
+    });
+
+    $('.pr_bar').each(function(){
+        let width = $(this).data('skill'); 
+        $(this).css('width', width + '%');
+    });
+
+    $('#menu_btn').click(function (){
+        $('#main_nav').toggleClass('open');
+    })
+});
